f8ac6fe6c9ae4c63d97d52105784c10386891fec
Микаил Абышов <mikailabysov@MacBook-Air-Mikail.local>
2024-08-11 00:11:12 +0300

    Some comments added

--- Application/Services/Implementations/EventService.cs
+++ Application/Services/Implementations/EventService.cs
@@ -4,13 +4,24 @@ using Domain.Entities;
 
 namespace Application.Services.Implementations;
 
+/// <summary>
+/// Сервис для работы с событиями
+/// </summary>
+/// <param name="repository"></param>
 public class EventService(IEventRepository repository) : IEventService 
 {
+  /// <summary>
+  /// Получить событие по Id
+  /// </summary>
+  /// <param name="id">Id события</param>
   public async Task<Event> GetById(Guid id)
   {
     return await repository.GetById(id);
   }
 
+  /// <summary>
+  /// Создать событие
+  /// </summary>
   public async Task<Guid> Create(EventInfo info)
   {
     var newEvent = Event.Create(info);
@@ -20,6 +31,11 @@ public class EventService(IEventRepository repository) : IEventService
     return newEvent.Id;
   }
 
+  /// <summary>
+  /// Обновить событие
+  /// </summary>
+  /// <param name="description">Описание для обновления</param>
+  /// <param name="eventId">Id события</param>
   public async Task<Event> UpdateInfo(string description, Guid eventId)
   {
     var updatingEvent = await repository.GetById(eventId);
@@ -31,5 +47,9 @@ public class EventService(IEventRepository repository) : IEventService
     return updatingEvent;
   }
 
+  /// <summary>
+  /// Удалить событие
+  /// </summary>
+  /// <param name="id">Id удаляемого события</param>
   public async Task Delete(Guid id) => await repository.Delete(id);
 }
